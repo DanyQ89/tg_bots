94b18e912aa8b6aedc8e93dbbc2ff98469f10771
DanyQ89 <108182297+DanyQ89@users.noreply.github.com>
2024-04-05 16:03:04 +0300

    data and time bot

--- Test.py
+++ Test.py
@@ -3,6 +3,8 @@ from aiogram.client.bot import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.filters import Command
 from aiogram.types import Message
+import datetime
+import time
 
 # from aiogram.utils.keyboard import ReplyKeyboardBuilder
 # from aiogram.fsm.state import State, StatesGroup
@@ -15,10 +17,21 @@ token = '7167007472:AAEvxsCaPQWFNiDjVJiYBeARvO1vJXq3KaQ'
 form_router = Router()
 
 
+@form_router.message(Command('time'))
+async def show_time(message: Message):
+    await message.answer(time.asctime().split()[3])
+
+
+@form_router.message(Command('date'))
+async def show_date(message: Message):
+    await message.answer(str(datetime.date.today()))
+
+
 @form_router.message(Command('start'))
 @form_router.message(F.text)
-async def answer(message:Message):
+async def answer(message: Message):
     await message.answer(f'Я получил сообщение {message.text}')
+
 
 async def main():
     bot = Bot(token=token, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
